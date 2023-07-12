f3fd7b42b790097ab489174aa9a69c2db84c97c7
HENRIQUE BATISTA <85231417+henbatista@users.noreply.github.com>
2023-07-12 10:25:05 -0300

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,32 @@
+export default {
+  head: {
+    title: 'Ibook',
+    htmlAttrs: {
+      lang: 'pt'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Ibook - New way to read books online'
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  css: ['normalize.css/normalize.css', '@/assets/scss/base.scss'],
+  plugins: ['@/plugins/accessor'],
+  components: [{ path: '@/components', pathPrefix: false }],
+  buildModules: ['@nuxt/typescript-build', '@nuxtjs/style-resources'],
+  styleResources: {
+    scss: ['@/components/bosons/*.scss']
+  },
+  modules: ['@nuxtjs/axios', 'cookie-universal-nuxt'],
+  axios: {
+    baseURL: 'http://localhost:3333/'
+  },
+  build: {
+    extractCSS: true
+  }
+}
